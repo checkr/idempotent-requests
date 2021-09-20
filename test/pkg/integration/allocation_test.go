04cf1d2ddf1a9757f7380897e61dc3a543586741
Ivan Rylach <ivan.rylach@checkr.com>
2021-09-20 13:32:17 -0500

    add integration tests for negative cases

--- test/pkg/integration/allocation_test.go
+++ test/pkg/integration/allocation_test.go
@@ -1,11 +1,15 @@
 package integration
 
 import (
+	"checkr.com/idempotent-requests/pkg/views"
 	"checkr.com/idempotent-requests/tests/integation/pkg/utils"
 	"encoding/base64"
+	"encoding/json"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -41,5 +45,47 @@ func TestCaptures_Allocate(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	resp, err = httpClient.Do(allocationReq)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	var capture views.CaptureRecord
+	err = json.Unmarshal(bodyBytes, &capture)
+
+	assert.NoError(t, err)
+	assert.True(t, reflect.DeepEqual(capture.Response, utils.CaptureRecordPayload(idempotencyKey).(views.CaptureRecord).Response))
+
+}
+
+func TestCaptures_Allocate_Duplicate(t *testing.T) {
+
+	cfg := GetConfig()
+
+	CapturesV2URL := cfg.Host + CapturesV2Path
+
+	utils.WaitForServerToBeReady(cfg.Host)
+
+	httpClient := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	idempotencyKey := base64.URLEncoding.EncodeToString([]byte(uuid.New().String()))
+
+	allocationReq := utils.NewAllocateCaptureRequest(CapturesV2URL, utils.CaptureAllocatePayload(idempotencyKey))
+
+	resp, err := httpClient.Do(allocationReq)
+
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusAccepted, resp.StatusCode)
+
+	resp, err = httpClient.Do(allocationReq)
+
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusConflict, resp.StatusCode)
 
 }
---
+++ test/pkg/integration/record_test.go
@@ -0,0 +1,33 @@
+package integration
+
+import (
+	"checkr.com/idempotent-requests/tests/integation/pkg/utils"
+	"encoding/base64"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestCaptures_Record_Without_Allocation(t *testing.T) {
+
+	cfg := GetConfig()
+
+	CapturesV2URL := cfg.Host + CapturesV2Path
+
+	utils.WaitForServerToBeReady(cfg.Host)
+
+	httpClient := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	idempotencyKey := base64.URLEncoding.EncodeToString([]byte(uuid.New().String()))
+
+	captureRecordReq := utils.NewRecordCaptureRequest(CapturesV2URL, utils.CaptureRecordPayload(idempotencyKey))
+
+	resp, err := httpClient.Do(captureRecordReq)
+
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusForbidden, resp.StatusCode)
+}
