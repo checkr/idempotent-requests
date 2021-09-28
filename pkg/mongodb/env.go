1019b88b423b26460925c147287262613911f149
Ivan Rylach <ivan.rylach@checkr.com>
2021-09-28 11:11:31 -0500

    extract MongoDB configurations

--- pkg/mongodb/env.go
+++ pkg/mongodb/env.go
@@ -5,7 +5,9 @@ import (
 )
 
 type Config struct {
-	URI string `env:"MONGODB_URI" envDefault:"mongodb://root:password123@localhost:27017"`
+	URI        string `env:"MONGODB_URI" envDefault:"mongodb://root:password123@localhost:27017"`
+	Database   string `env:"MONGODB_DATABASE" envDefault:"idempotent-requests"`
+	Collection string `env:"MONGODB_COLLECTION" envDefault:"captures"`
 }
 
 func GetConfig() Config {
